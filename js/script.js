e5d08c6e07251729b064beb7bf38b9d9592dd46c
realsulty <Sultanobm1@gmail.com>
2023-12-19 23:49:17 +0000

    added tv show pages

--- js/script.js
+++ js/script.js
@@ -79,8 +79,12 @@ function hideSpinner(){
 
 async function diplsayMovieDetails() {
 const movieID = window.location.search.split('=')[1];
- 
+
 const movie = await fetchAPIData(`movie/${movieID}`);
+
+// Dipslay the overlay background image 
+// Here the functions is called up with Paramters that we init in function 
+displayBackgroundImage('movie', movie.backdrop_path)
 
 const div = document.createElement('div');
 div.innerHTML = `
@@ -137,6 +141,94 @@ document.querySelector('#movie-details').appendChild(div);
 
 }
 
+async function diplsayShowDetails() {
+    const showId = window.location.search.split('=')[1];
+    
+    const show = await fetchAPIData(`tv/${showId}`);
+    
+    // Dipslay the overlay background image 
+    // Here the functions is called up with Paramters that we init in function 
+    displayBackgroundImage('tv', show.backdrop_path)
+    
+    const div = document.createElement('div');
+    div.innerHTML = `
+     <div class="details-top">
+    <div>
+    ${
+        show.poster_path
+        ?`<img  
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+      : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${show.title}"
+    />`
+      }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)}/ 10
+      </p>
+      <p class="text-muted">First Air Date: ${show.first_air_date}</p>
+      <p>
+        ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+       ${show.genres.map((genre)=> `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+    </div>
+    </div>
+    <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}</li>
+      <li><span class="text-secondary">Last Episode to be Aired:</span> ${show.last_episode_to_air.name}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+    ${show.production_companies
+    .map((company) => `<span>${company.name}</span>`).join('')}</div>
+    </div>
+    
+    `;
+    
+    document.querySelector('#show-details').appendChild(div);
+    
+    }
+
+// Display Backdrop image on details Pages 
+// here we sit up the functions for mulitpile use in tv shows and movie pages as well
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div')
+    overlayDiv.style.backgroundImage = 
+    `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+overlayDiv.style.backgroundSize ='cover';
+overlayDiv.style.backgroundPosition = 'center';
+overlayDiv.style.backgroundRepeat = 'no-repeat';
+overlayDiv.style.height = '100vh';
+overlayDiv.style.width = '100vw'
+overlayDiv.style.position = 'absolute';
+overlayDiv.style.top = '0'
+overlayDiv.style.left ='0'
+overlayDiv.style.zIndex = '-1'
+overlayDiv.style.opacity = '0.1'
+
+if ( type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv)
+} else {  
+    document.querySelector('#show-details').appendChild(overlayDiv)
+}
+}
+
+
 
 // Fetch Data from TMDB API this can be referd to as a global fetch method
 async function fetchAPIData(endpoint) {
@@ -179,7 +271,7 @@ function init() {
              diplsayMovieDetails();
             break;
          case '/tv-details.html':
-            console.log('Tv Shows');
+             diplsayShowDetails();
             break;
          case '/search.html':
             console.log('Serach Bar');
